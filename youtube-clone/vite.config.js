068c16ad8ae198c23c1e2d54e7fcd4ccd85f6c14
BeastZoro <126886437+BeastZoro@users.noreply.github.com>
2023-11-11 15:38:08 +0530

    Final build

--- youtube-clone/vite.config.js
+++ youtube-clone/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+    'process.env.VITE_API_URL': JSON.stringify(process.env.VITE_API_URL),
+  },
 })
