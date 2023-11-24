243d7c3087150a218b1a815ad4f3cae236b4f039
BeastZoro <126886437+BeastZoro@users.noreply.github.com>
2023-11-24 17:57:57 +0530

    all changes

---
+++ awakener/src/App.jsx
@@ -0,0 +1,16 @@
+import { useEffect, useState } from "react";
+
+import "./App.css";
+
+function App() {
+  
+  const [FirstState, setFirstState] = useState('')
+
+  return (
+    <>
+      <p>Hello App</p>
+    </>
+  );
+}
+
+export default App;
